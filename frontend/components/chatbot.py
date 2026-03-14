6330435059f1bede5a04cd0f13dc37bbcaabaa58
Vaishnavi Sainath Pachange <vaishnavipachange714@gmail.com>
2026-03-14 17:30:32 +0530

    Add files via upload

---
+++ frontend/components/chatbot.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from gtts import gTTS
+import base64
+import os
+
+def speak(text):
+    tts = gTTS(text)
+    tts.save("voice.mp3")
+
+    audio_file = open("voice.mp3", "rb").read()
+    st.audio(audio_file, format="audio/mp3")
+
+    # Remove voice file after playing
+    if os.path.exists("voice.mp3"):
+        os.remove("voice.mp3")
+
+
+def show_explanation(text):
+    st.markdown(
+        f"""
+        <div style="
+            background:#1e1e1e;
+            padding:20px;
+            border-radius:10px;
+            font-size:16px;
+            color:white;
+            max-width: 600px;
+            line-height: 1.5;
+            word-wrap: break-word;
+            white-space: normal;
+            margin-bottom: 20px;
+        ">
+            <b>AI Explanation:</b><br><br>{text}
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
---
+++ frontend/components/form_inputs.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+def user_form():
+    st.markdown('<div class="card">', unsafe_allow_html=True)
+    st.markdown("### 🏡 Enter House Features")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        MedInc = st.number_input("Median Income")
+        HouseAge = st.number_input("House Age")
+        AveRooms = st.number_input("Average Rooms")
+        AveBedrms = st.number_input("Average Bedrooms")
+
+    with col2:
+        Population = st.number_input("Population")
+        AveOccup = st.number_input("Average Occupancy")
+        Latitude= st.number_input("Latitude")   
+
+    st.markdown('</div>', unsafe_allow_html=True)
+
+    return {
+        "MedInc": MedInc,
+        "HouseAge": HouseAge,
+        "AveRooms": AveRooms,
+        "AveBedrms": AveBedrms,
+        "Population": Population,
+        "AveOccup": AveOccup,
+        "Latitude": Latitude
+    }
